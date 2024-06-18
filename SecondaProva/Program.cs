25f89fede5074b06758a9b3b52feefc517204f79
Luca-pintimalli <163635386+Luca-pintimalli@users.noreply.github.com>
2024-06-18 16:54:01 +0200

    .

--- SecondaProva/Program.cs
+++ SecondaProva/Program.cs
@@ -1,10 +1,70 @@
 ﻿namespace SecondaProva;
 
+ class Persona
+{
+    //ATTRIBUTI CLASSI  
+    string nome = "";
+    string cognome = "";
+    int  eta;
+
+    //proprieta' delle classi
+    public string Nome
+    {
+        get { return nome; }
+        set { nome = value; }
+    }
+
+    public string Cognome
+    {
+        get { return cognome; }
+        set { cognome = value; }
+
+    }
+    public int Eta
+    {
+        get { return eta; }
+        set { eta = value; }
+    }
+
+    //METODI DELLA CLASSE
+
+    public string GetNome()
+    {
+        return nome;
+    }
+
+    public string GetCognome()
+    {
+        return cognome;
+    }
+
+    public int GetEta()
+    {
+        return eta;
+
+    }
+
+    public string GetDettagli()
+    {
+        return $"Nome:{nome}, Cognome:{cognome}, Eta:{eta}";
+    }
+    
+
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Persona p = new Persona();
+        p.Nome = "Mario";
+        p.Cognome = "Verdi";
+        p.Eta = 30;
+
+        Console.WriteLine(p.GetNome());
+        Console.WriteLine(p.GetCognome());
+        Console.WriteLine(p.GetEta());
+        Console.WriteLine(p.GetDettagli());
     }
 }
 
